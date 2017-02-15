e67edf1eb1c50191d5ed3a43a47af47b1a22b993
zxymike <zxymike93@gmail.com>
2017-02-15 13:10:20 +0800

    EOF 的值

--- eof.c
+++ eof.c
@@ -2,7 +2,9 @@
 
 main()
 {
+    /* -1 */
     printf("The value of EOF is %d\n\n", EOF);
+    /* press control+D */
     printf("Enter a character: \n");
     printf("The expression 'getchar() != EOF' evaluates to %d\n", getchar() != EOF);
 }
