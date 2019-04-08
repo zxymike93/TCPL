6049944cf408fa6718a680e21d0eae531faeb129
zxymike <zxymike93@gmail.com>
2019-04-08 10:51:22 +0800

    chapter3, 1~6; chapter-4, 1~6, 11~14

--- chapter-3/3_1.c
+++ chapter-3/3_1.c
@@ -1,18 +1,55 @@
+#include <stdio.h>
+
+// Return index of x in s(a list of numbers)
 int binsearch(int x, int s[], int len)
 {
-    int low, mid, high;
-    low = 0;
-    high = len - 1;
-    mid = (low + high) / 2;
-    while (low <= high && x != s[mid]) {
-        if (x < s[mid])
-            high = mid - 1;
+        int low = 0;
+        int high = len - 1;
+        int mid = (low + high) / 2;
+
+        while (low <= high && x != s[mid]) {
+                if (x < s[mid])
+                        high = mid - 1;
+                else
+                        low = mid + 1;
+
+                mid = (low + high) / 2;
+        }
+
+        if (x == s[mid])
+                return mid;
         else
-            low = mid + 1;
-        mid = (low + high) / 2;
-    }
-    if (x == s[mid])
-        return mid;
-    else
-        return -1;
+                return -1;
+}
+
+int test_one()
+{
+        int len = 1000;
+        int nums[len];
+        int i = 0;
+
+        while (i < len -1) {
+                nums[i] = i + 1;
+                i++;
+        }
+
+        return binsearch(5, nums, len);
+}
+
+int test_two()
+{
+        int x = 4;
+        int nums[10] = {0, 4, 8, 9, 10, 11, 14, 35, 100, 111};
+
+        return binsearch(8, nums, 10);
+}
+
+int main()
+{
+        int r;
+
+        r = test_one();
+        printf("Test binsearch: %d\n", r);
+        r = test_two();
+        printf("Test binsearch: %d\n", r);
 }
--- chapter-3/3_2.c
+++ chapter-3/3_2.c
@@ -1,46 +1,47 @@
 int escape(char s[], char t[])
 {
-    int i, j;
+        int i, j;
 
-    for (i = j = 0; s[i] != '\0'; ++i, ++j) {
-        switch (s[i]) {
-        case '\n':
-            t[j++] = '\\';
-            t[j] = 'n';
-            break;
-        case '\t':
-            t[j++] = '\\';
-            t[j] = 't';
-            break;
-        default:
-            t[j] = s[i];
-            break;
+        for (i = j = 0; s[i] != '\0'; ++i, ++j) {
+                switch (s[i]) {
+                case '\n':
+                        t[j++] = '\\';
+                        t[j] = 'n';
+                        break;
+                case '\t':
+                        t[j++] = '\\';
+                        t[j] = 't';
+                        break;
+                default:
+                        t[j] = s[i];
+                        break;
+                }
         }
-    }
-    t[j] = '\0';
-}
 
+        t[j] = '\0';
+}
 
 int unescape(char s[], char t[])
 {
-    int i, j;
+        int i, j;
 
-    for (i = j = 0; s[i] != '\0'; ++i, ++j) {
-        if (s[i] != '\\')
-            t[j] = s[i];
-        else {
-            switch (s[++i]) {
-            case 't':
-                t[j] = '\t';
-                break;
-            case 'n':
-                t[j] = '\n';
-                break;
-            default:
-                t[j] = s[i];
-                break;
-            }
+        for (i = j = 0; s[i] != '\0'; ++i, ++j) {
+                if (s[i] != '\\') {
+                        t[j] = s[i];
+                } else {
+                        switch (s[++i]) {
+                        case 't':
+                                t[j] = '\t';
+                                break;
+                        case 'n':
+                                t[j] = '\n';
+                                break;
+                        default:
+                                t[j] = s[i];
+                                break;
+                        }
+                }
         }
-    }
-    t[j] = '\0';
+
+        t[j] = '\0';
 }
